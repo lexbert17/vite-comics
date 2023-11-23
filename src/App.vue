7d9684832120b3560a4db84750a33b864ffe0e31
lexbert17 <lexbertlazo@gmail.com>
2023-11-23 17:07:14 +0100

    add: creazione pagina header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script>
+import AppHeader from "./components/appHeader.vue"
+import AppContent from "./components/AppContent.vue";
+import AppNavigation from "./components/AppNavigation.vue"
 
+export default {
+  components: {
+    AppHeader,
+    AppContent,
+    AppNavigation
+  },
+}
 </script>
 
 <template>
- 
+
+<AppHeader/>
 </template>
 
-<style scoped>
-
+<style lang="scss">
+@use "./style/general.scss";
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,66 @@
+<script>
+    export default {
+        data() {
+            return {
+                navHeader: [ "CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"]
+            }
+        }
+    }
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <div>
+                <img src="../assets/img/dc-logo.png" alt="img">
+            </div>
+            <ul>
+                <li v-for="item in navHeader"><a href="">{{ item }}</a></li>
+            </ul>
+        </div>    
+    </header>
+</template>
+
+<style lang="scss" scoped>
+.container{
+    display: flex;
+    width: 80%;
+    margin: auto;
+    justify-content: space-between;
+    margin: auto;
+    background-color: green;
+    height: 120px;
+    align-items: center;
+    
+  
+    img {
+        width: 80px;
+    }
+
+    ul {
+        display: flex;
+        gap: 1rem;
+        list-style: none;
+        background-color: orange;
+        height: 100%;
+        
+
+        a {
+        color: black;
+        text-decoration: none;
+        display: block;
+        line-height: 120px;
+        height: 120px;
+       
+        
+
+            &:hover {
+                color: hsl(208.67deg 100% 48.82%);
+                border-bottom: 5px solid hsl(208.67deg 100% 48.82%);
+         
+            }
+          
+        }
+    }
+};   
+</style> 
